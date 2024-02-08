425820d948acebdfee10159f56bdf7bba8e2271f
Fraulo23 <fraulinifederico@gmail.com>
2024-02-08 17:36:22 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vettore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector v1 = new Vector(15, 10);
+            Vector v2=new Vector(30, 10);
+            Console.WriteLine(v1+v2);
+            Console.WriteLine(v1-v2);
+            Console.WriteLine(v1*v2);
+            Console.WriteLine(v1/v2);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Vector.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vettore
+{
+    internal class Vector
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public Vector(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+        public override string ToString()
+        {
+            return X+";"+Y;
+        }
+        public static Vector operator +(Vector a, Vector b)
+        {
+            return new Vector(a.X+b.X, a.Y+b.Y);
+        }
+        public static Vector operator -(Vector a, Vector b)
+        {
+            return new Vector(a.X-b.X, a.Y-b.Y);
+        }
+        public static Vector operator *(Vector a, Vector b)
+        {
+            return new Vector(a.X*b.X, a.Y*b.Y);
+        }
+        public static Vector operator /(Vector a, Vector b)
+        {
+            return new Vector(a.X / b.X, a.Y / b.Y);
+        }
+    }
+}
