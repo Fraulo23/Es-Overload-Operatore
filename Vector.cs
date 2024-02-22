f15da3367ec20cb7a317016191c2b049cf65521a
Fraulo23 <fraulinifederico@gmail.com>
2024-02-22 17:41:40 +0100

    Aggiunta versore

--- Vector.cs
+++ Vector.cs
@@ -19,6 +19,35 @@ namespace Vettore
         {
             return X+";"+Y;
         }
+        public static bool operator ==(Vector f, Vector f2)
+        {
+            if (object.ReferenceEquals(f, null))
+                return object.ReferenceEquals(f2, null);
+            else if (object.ReferenceEquals(f2, null))
+                return false;
+            else
+                return f.X == f2.X && f.Y == f2.Y;
+        }
+        public static bool operator !=(Vector f, Vector f2)
+        {
+            return !(f == f2);
+        }
+        public static bool operator >(Vector f, Vector f2)
+        {
+            return f.Modulo() > f2.Modulo();
+        }
+        public static bool operator <(Vector f, Vector f2)
+        {
+            return f.Modulo() < f2.Modulo();
+        }
+        public static bool operator >=(Vector f, Vector f2)
+        {
+            return f.Modulo() >= f2.Modulo();
+        }
+        public static bool operator <=(Vector f, Vector f2)
+        {
+            return f.Modulo() <= f2.Modulo();
+        }
         public static Vector operator +(Vector a, Vector b)
         {
             return new Vector(a.X+b.X, a.Y+b.Y);
@@ -54,6 +83,25 @@ namespace Vettore
         public static Vector operator +(Vector a)
         {
             return (a);
+        }
+        public double Modulo()
+        {
+            return Math.Sqrt(X*X+Y*Y);
+        }
+        public Vector ValoreAssoluto()
+        {
+            if (this.Modulo() < 0)
+            {
+                return this * (-1);
+            }
+            else
+            {
+                return this;
+            }
+        }
+        public Vector Versore()
+        {
+            return this/this.ValoreAssoluto();
         }
 
     }
